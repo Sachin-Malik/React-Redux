29945c55eb75b461f49e503e3a6c9644eea127ed
Sachin Malik <sachinmalik199821@gmail.com>
2021-10-23 11:35:10 +0530

    Add Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Switch,Route,Link} from 'react-router-dom';
+
 import IceCreamContainer from './components/IceCreamContainer';
 import CoffeeContainer from './components/CoffeeContainer';
 import NewCakeContainer from './components/NewCakeContainer';
@@ -10,9 +13,32 @@ function App() {
   return (
     <Provider store={store}>
       <div>
-        <NewCakeContainer />
-        <IceCreamContainer />
-        <CoffeeContainer />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Buy Cake</Link>
+          </li>
+          <li>
+            <Link to="/icecream">Buy IceCream</Link>
+          </li>
+          <li>
+            <Link to="/coffee">Buy Coffee</Link>
+          </li>
+        </ul>
+       
+        <Switch>
+            <Route exact path="/" >
+              <NewCakeContainer />
+            </Route>
+            <Route  path="/icecream" >
+             <IceCreamContainer />
+            </Route>
+            <Route exact path="/coffee">
+              <CoffeeContainer />
+            </Route>
+        </Switch>
+
+        </Router>
       </div>
     </Provider>
   );
